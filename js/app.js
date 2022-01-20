0f730765ee7879ca5cde5e649d39daa63b45a025
danilsonplacid <denilsonplacid@gmaile.com>
2022-01-20 13:56:16 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,60 @@
+function getPin() {
+    const pin =Math.round(Math.random() * 10000);
+    const pinString = pin + ''; 
+    if (pinString.length == 4) {
+        return pin;
+    }
+    else{
+        return getPin();
+    }
+};
+function generatePin() {
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+};
+
+
+// Calc and pin matcher 
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else{
+        const preNum = calcInput.value;
+        const newNum =  preNum + number;  
+        calcInput.value = newNum;
+    }
+    
+});
+
+function verifyPin() {
+
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const success =  document.getElementById('notify-success');
+    const failError =  document.getElementById('notify-fail');
+
+    if (pin == typedNumbers) {
+        
+        success.style.display = 'block';
+        failError.style.display = 'none';
+    }
+    else{
+        
+        failError.style.display = 'block';
+        success.style.display = 'none';
+    }
+
+    if (typedNumbers == "") {
+        alert('Empty Box. Please Try Again');
+    }
+
+    else{
+        console.log('Number of attemps left');
+    }
+};
